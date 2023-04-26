297867d5e62046bdbfaa6428e9b1fe0d1b39d64a
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-04-26 14:36:49 +0530

    added iife function for database

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -3,11 +3,29 @@ import InstructionsComponent from "../components/InstructionsComponent";
 import NftGallery from "../components/nftGallery.jsx";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import { useAccount } from "wagmi";
+import { useEffect,useState } from "react";
 
 export default function Home() {
-  // useEffect(() => {
+  const { address, isConnecting, isDisconnected } = useAccount()
 
-  // }, [address])
+  useEffect(() => {
+    if(!address) return 
+    ;(async ()=>{
+      // iife - immediately inwoked functional expression
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+
+      const result = await client.createIfNotExists(userDoc)
+
+      welcomeUser(result.userName)
+    })()
+  }, [address])
+
   return (
     <div>
       <Header />
